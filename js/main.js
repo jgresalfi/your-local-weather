c733b6a1b0d4b92e9b80bd65c37cebc646d3210d
Jason Gresalfi <jason.gresalfi@gmail.com>
2016-09-30 14:54:17 -0500

    Wired up Open Weather API and tested

--- js/main.js
+++ js/main.js
@@ -5,8 +5,9 @@
 // APPID {APIKEY} is your unique API key
 
 $(document).ready(function() {
-	var apiKey = 'http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=' + '664847130300b965105640259c4f4829';
+	var apiKey = 'http://api.openweathermap.org/data/2.5/weather?id=2172797&APPID=' + '664847130300b965105640259c4f4829';
 	$.getJSON(apiKey, function(data) {
-		$("#main-icon").html("<img src='" + data.weather.icon + "'");
-	})
+		console.log(data);
+		$("#main-icon").html("<h2>" + data.weather[0].description + "</h2>");
+	});
 })
