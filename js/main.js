9c3250a8d7ce564e3faf15c34a8f86db1c954b2c
Jason Gresalfi <jason.gresalfi@gmail.com>
2016-09-30 21:02:45 -0500

    Wired up geolocation for API and tested

--- js/main.js
+++ js/main.js
@@ -5,9 +5,19 @@
 // APPID {APIKEY} is your unique API key
 
 $(document).ready(function() {
-	var apiKey = 'http://api.openweathermap.org/data/2.5/weather?id=2172797&APPID=' + '664847130300b965105640259c4f4829';
-	$.getJSON(apiKey, function(data) {
-		console.log(data);
-		$("#main-icon").html("<h2>" + data.weather[0].description + "</h2>");
-	});
+    var apiKey,
+        lat,
+        lon;
+
+    function success(position) {
+        lat = position.coords.latitude;
+        lon = position.coords.longitude;
+        apiKey = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + '664847130300b965105640259c4f4829';
+        $.getJSON(apiKey, function(data) {
+            console.log(data);
+            $("#main-icon").html("<h2>" + data.weather[0].description + "</h2>");
+        });
+    }
+
+    navigator.geolocation.getCurrentPosition(success);
 })
